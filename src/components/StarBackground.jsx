b60105ab1a93cec7f99c1e1cf50645d2b16c7e82
Auspicious77 <elishaibukun@gmail.com>
2025-08-21 15:36:13 +0100

    Increase meteor y-coordinate range for enhanced visual effect

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -50,7 +50,9 @@ export const StarBackground = () => {
         id: i,
         size: Math.random() * 2 + 1,
         x: Math.random() * 100,
-        y: Math.random() * 20,
+        y: Math.random() * 100,
+
+        // y: Math.random() * 20,
         delay: Math.random() * 15,
         animationDuration: Math.random() * 3 + 3,
       });
