694fac80b4592026728b5f44476895a04e28cd0a
Auspicious77 <elishaibukun@gmail.com>
2025-08-21 13:41:45 +0100

    Refactor routing to use HashRouter and update project links

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
@@ -8,13 +8,13 @@ function App() {
   return (
     <>
       <Toaster />
-      <Router>
+      <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
             <Route path="/projects" element={<ProjectsPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </>
   );
 }
